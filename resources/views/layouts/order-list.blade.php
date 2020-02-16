3591b002d9f240674a2ff1c58d6ddfc8ff430607
Tohidul Islam Foysal <foysal11203004@gmail.com>
2020-02-16 18:28:56 +0600

    init repo

---
+++ resources/views/layouts/order-list.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+
+@section('content')
+    <p>this is order list</p>
+@endsection
---
+++ resources/views/layouts/product-list.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+
+@section('content')
+<p>this is product list</p>
+<table class="table table-stripped">
+    <tr>
+        <td>hello</td>
+    </tr>
+</table>
+@endsection
